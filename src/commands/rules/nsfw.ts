49b8c3b05e41cb2aae2c41a5a04b80e12d7be45a
N-Pelletant <pelletantnic@gmail.com>
2023-04-22 20:37:14 +0200

    fix: nsfw command

--- src/commands/rules/nsfw.ts
+++ src/commands/rules/nsfw.ts
@@ -1,9 +1,18 @@
-import { SlashCommandBuilder, CommandInteraction, channelMention } from "discord.js";
+import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
-export const data: SlashCommandBuilder = new SlashCommandBuilder().setName('insults').setDescription('Quotes the insults rule');
+export const data: SlashCommandBuilder = new SlashCommandBuilder().setName('nsfw').setDescription('Quotes the nsfw rule');
 export const execute = async (interaction: CommandInteraction) => {
+  const nsfwId = interaction.guild?.channels.cache.get("1044321688717889606")?.toString();
+
+  if (nsfwId === undefined) {
+    await interaction.reply(`
+>>> ❌ **NO** NSFW outside of the nsfw channels
+❌ **PAS** de NSFW en dehors des channels nsfw
+  `);
+  }
+
   await interaction.reply(`
->>> ❌ **NO** NSFW outside of the ${channelMention("nsfw")} channel
-❌ **PAS** de NSFW en dehors du channel ${channelMention("nsfw")}
+>>> ❌ **NO** NSFW outside of the ${nsfwId!} channel
+❌ **PAS** de NSFW en dehors du channel ${nsfwId!}
 `);
 };
