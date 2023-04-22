fc7cd371ab433b5bb6221586b79b9bbb0ded60d2
N-Pelletant <pelletantnic@gmail.com>
2023-04-22 20:27:59 +0200

    fix: art-critic command

--- src/commands/rules/art-critic.ts
+++ src/commands/rules/art-critic.ts
@@ -1,9 +1,9 @@
-import { SlashCommandBuilder, CommandInteraction, channelMention } from "discord.js";
+import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
 export const data: SlashCommandBuilder = new SlashCommandBuilder().setName('art-critics').setDescription('Quotes the art critics rule');
 export const execute = async (interaction: CommandInteraction) => {
   await interaction.reply(`
->>> ❌ **NO** art critic unless specifically in the ${channelMention("help-and-art-critic")} channel
-❌ **PAS** de critique d'art en dehors du channel ${channelMention("help-and-art-critic")}
+>>> ❌ **NO** art critic unless specifically asked for it and be respectful
+❌ **PAS** de critique d'art à moins d'une demande explicite, restez respectueux
 `);
 };
